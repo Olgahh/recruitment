c2d4262b69e1f53995fbd028c648fa27eb8aaa23
Olga <hijazin.olga@gmail.com>
2020-02-06 13:34:43 -0800

    Modified Recruitment Task

--- recruitment.py
+++ recruitment.py
@@ -1,5 +1,29 @@
 def main():
-	#write your code here
+    #write your code here
+    skills = ["Python", "C++", "JavaScript", "Meeting", "Leeting", "Eating"]
+    cv = {}
+    cv['name'] = input("What's your name? ")
+    cv['age'] =int(input("How old are you? "))
+    cv['experience'] =int (input("How many years of experience do you have? "))
+    cv['skills'] = []
+
+    print("Skills:")
+    
+    number = 1
+    for skill in skills:
+        print("{}. {}".format(number , skill))
+        number +=1
+    cv['skills'] =[int(input("Choose a skill from above by entering its number: "))]
+    cv['skills'].extend([int(input("Choose another skill from above by entering its number: "))])
+
+    if(6 in cv['skills']):
+        if (cv['age'] >= 25 and cv['age'] < 40 and cv['experience'] > 5):
+            print("accepted")
+        else:
+            print("rejected")
+    else:
+        print("rejected")
+
 
 if __name__ == '__main__':
-	main()
+    main()
